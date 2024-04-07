41a389bcfefe63f0dbb2a87fb178b126ecf91e52
parijatkawale <parijat.kawale@gmail.com>
2024-04-07 10:00:19 -0400

    Fixed flex issue for footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -25,12 +25,12 @@ const Footer = () => {
               <div className="bg-neutral-700 my-5 h-[1px] w-full" />
               <div className="flex md:flex-row flex-col md: items-center justify-between">
                   <p className="font-semibold text-gray text-xs">Copyright @ 2024 Apple Inc. All rights reserved. </p>
-                  <div className="flex">
+                  <div className="flex flex-wrap">
                       {footerLinks.map((link, i) => (
                           <p key={link} className="font-semibold text-gray text-xs">
                               {link} {' '}
                               {i !== footerLinks.length - 1 && (
-                                  <span className="mx-2">|</span>
+                                  <span className="px-2">|</span>
                               )}
                           </p>
                       ))}
